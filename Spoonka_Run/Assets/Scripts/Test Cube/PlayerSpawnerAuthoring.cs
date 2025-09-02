3fa2f637162047f60dfa13563f77ebe7988a0917
SpoonKa <spoon1998spoon@gmail.com>
2025-09-02 21:58:08 +0200

    Cube 1 Test System

---
+++ Spoonka_Run/Assets/Scripts/Test Cube/PlayerSpawnerAuthoring.cs
@@ -0,0 +1,25 @@
+﻿
+using Unity.Entities;
+using UnityEngine;
+
+public struct PlayerSpawner : IComponentData
+{
+    public Entity PalyerEntity;
+}
+
+[DisallowMultipleComponent]
+public class PlayerSpawnerAuthoring : MonoBehaviour
+{
+    public GameObject PalyerGameOb;
+
+    class Baker : Baker<PlayerSpawnerAuthoring>
+    {
+        public override void Bake(PlayerSpawnerAuthoring authoring)
+        {
+            PlayerSpawner component = default(PlayerSpawner);
+            component.PalyerEntity = GetEntity(authoring.PalyerGameOb, TransformUsageFlags.Dynamic);
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent(entity, component);
+        }
+    }
+}
