ef877b23321adefd81a5491afa5c03bfcbcc1075
SpoonKa <spoon1998spoon@gmail.com>
2025-09-05 20:16:57 +0200

    Finish First Mein Ui  ( Menu )

---
+++ Spoonka_Run/Assets/Scripts/UI/MainMenu/UIMeniMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMeniMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _options;
+   
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Options() 
+    {
+        _options.SetActive(true);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // Закрывает билд
+#endif
+    }
+    public void Update()
+    {
+
+        if (_options.activeSelf &&  Input.GetKey(KeyCode.Escape))
+        {
+            _options.SetActive(false);
+            return;
+        }
+    }
+}
